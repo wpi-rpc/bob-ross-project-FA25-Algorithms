7ddc1dffc01a8a599774aa6dc01a78aa2f6a8230
Omar Suazo <omar@Omars-MacBook-Pro-3107.local>
2025-11-15 10:34:32 -0500

    Added coloring functionality to the drawing

--- image-processing.py
+++ image-processing.py
@@ -1,16 +1,16 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-from path_planning import path_coordinates
+from edge_path_planning import edge_path_coordinates
 
-img = cv.imread('circles.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('pikachu.webp', cv.IMREAD_GRAYSCALE)
+bgr_img = cv.imread('pikachu.webp')
 assert img is not None, "file could not be read, check with os.path.exists()"
 edges = cv.Canny(img,150,400)
+rgb_image = bgr_img[:, :, ::-1]
+path = edge_path_coordinates(edges)
 
-path = path_coordinates(edges)
-print(path)
-
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.subplot(121),plt.imshow(rgb_image)
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
