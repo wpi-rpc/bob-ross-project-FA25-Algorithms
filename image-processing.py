3d202d94fecc49904ab0a0574bb6e91817d33942
Omar Suazo <omar@Omars-MacBook-Pro-3107.local>
2025-11-15 10:40:06 -0500

    Solving conflicts

--- image-processing.py
+++ image-processing.py
@@ -1,19 +1,18 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-from edge_path_planning import edge_path_coordinates
+from path_planning import path_coordinates
 
-img = cv.imread('pikachu.webp', cv.IMREAD_GRAYSCALE)
-bgr_img = cv.imread('pikachu.webp')
+img = cv.imread('circles.jpg', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 edges = cv.Canny(img,150,400)
-rgb_image = bgr_img[:, :, ::-1]
-path = edge_path_coordinates(edges)
 
-plt.subplot(121),plt.imshow(rgb_image)
+path = path_coordinates(edges)
+print(path)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
  
 plt.show()
-
