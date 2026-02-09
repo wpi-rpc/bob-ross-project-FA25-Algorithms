e097c15bcc2a10c4bf67c9e11d656cd47e404fc8
Emerson936 <57594164+Emerson936@users.noreply.github.com>
2026-02-09 10:52:21 -0500

    Add files via upload

---
+++ bob-ross-project-FA25-Algorithms-main/cpp/build/CMakeFiles/BobRoss.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BobRoss"
+  "BobRoss.pdb"
+  "CMakeFiles/BobRoss.dir/csv_write.cpp.o"
+  "CMakeFiles/BobRoss.dir/csv_write.cpp.o.d"
+  "CMakeFiles/BobRoss.dir/edge_path_planning.cpp.o"
+  "CMakeFiles/BobRoss.dir/edge_path_planning.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BobRoss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
